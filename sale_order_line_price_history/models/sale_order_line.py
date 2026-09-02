4b2333fb1b81a57562ddd4b72a143cad93d8b759
arsh.qq <arsh.bringer@yahoo.com>
2026-09-02 04:47:49 +0000

    [FEATURE] add purchase and sale history qol

---
+++ sale_order_line_price_history/models/sale_order_line.py
@@ -0,0 +1,13 @@
+# Copyright 2021 Tecnativa - David Vidal
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import fields, models
+
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    # In core this a related field. We need to trigger its value on view, so we can
+    # have it even when we're in a NewId. The original dependency must be kept as
+    # well, otherwise the stored value isn't recomputed when the order partner
+    # changes (an explicit `depends` fully replaces the related one).
+    order_partner_id = fields.Many2one(depends=["order_id.partner_id", "product_id"])
